b4e70631a59de1faf558de2b1374d5e3b7e2172c
adash333 <adash333@hotmail.com>
2019-02-09 04:40:50 +0900

    bulma and vuex

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="hero is-primary is-bold">
+    <div class="hero-body">
+      <h1 class="title is-size-2">About</h1>
+      <h2 class="subtitle is-size-4">NBH3とは？</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  fetch({ store }) {
+    store.commit("resetMenu");
+  }
+};
+</script>
